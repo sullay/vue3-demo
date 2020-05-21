d320a4bb3b83d6f486862b90f5302a0a0792a333
sullay <1181518458@qq.com>
2020-05-21 10:34:39 +0800

    s-timeline

---
+++ src/components/timeline/Main.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="s-timeline">
+    <div class="s-timeline-item" v-for="(val,i) in list" :key="i">
+      <div v-if="i!==list.length-1" class="s-timeline-item-tail"></div>
+      <div class="s-timeline-item-node"></div>
+      <div class="s-timeline-item-wrapper">
+        1111111111111111111111
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    list: []
+  },
+  setup () {
+
+  }
+}
+</script>
+<style lang="scss" scoped>
+.s-timeline{
+  .s-timeline-item{
+    position: relative;
+    padding-bottom: 20px;
+    .s-timeline-item-tail{
+      position: absolute;
+      left: 4px;
+      height: 100%;
+      border-left: 2px solid #e4e7ed;
+    }
+    .s-timeline-item-node{
+      position: absolute;
+      // background-color: #e4e7ed;
+      background-image: url("./img/笑脸.svg");
+      background-position: center;
+      background-size: 100%;
+
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      left: -1px;
+      width: 12px;
+      height: 12px;
+    }
+    .s-timeline-item-wrapper{
+      position: relative;
+      padding-left: 28px;
+      top: -3px;
+      text-align: left;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="home">
     <button @click="click">button</button>
+    <s-timeline :list="list"></s-timeline>
   </div>
 </template>
 
 <script>
 import { message } from '../components/message/index'
+import sTimeline from '../components/timeline/Main'
 export default {
   name: 'Home',
+  components: {
+    sTimeline
+  },
   setup () {
-    function click () {
-      message({ text: '11111111111111111111111', type: 'danger' })
-    }
-    return { click }
+    const list = [{}, {}, {}]
+    return { click, list }
   }
+}
+function click () {
+  message({ text: '11111111111111111111111', type: 'danger' })
 }
 </script>
 <style lang="scss" scoped>
@@ -21,5 +27,9 @@ button{
   position: absolute;
   top: 10px;
   left: 10px;
+}
+.s-timeline{
+  width: 500px;
+  margin: 0 auto;
 }
 </style>
